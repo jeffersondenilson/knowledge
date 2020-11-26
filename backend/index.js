f66db5627808bc51aacaaf86f55b0f8578cad372
jeffersondenilson <jefersond121212@gmail.com>
2020-11-26 11:08:48 -0300

    primeiro commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const PORT = process.env.PORT || 5000;
+const app = require('express')();
+const consign = require('consign');
+const db = require('./config/db');
+
+app.db = db;
+
+consign()
+	.then('./config/middleware.js')
+	.then('./api/validators.js')
+	.then('./api')
+	.then('./config/routes.js')
+	.into(app);
+
+app.listen(PORT, ()=>console.log(`Listening on port ${PORT}...`));uire()
