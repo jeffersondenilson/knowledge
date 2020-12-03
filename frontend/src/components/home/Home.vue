98cbbaaec48ecd0171f9ec6664ecd0a135b3b465
jeffersondenilson <jefersond121212@gmail.com>
2020-12-03 20:32:06 -0300

    stats

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,18 +1,66 @@
 <template>
 	<div class="home">
-		<PageTitle 
-			icon="fa fa-home" 
-			title="Dashboard" 
+		<PageTitle
+			icon="fa fa-home"
+			title="Dashboard"
 			subtitle="Base de Conhecimento"
 		/>
+		<div class="stats">
+			<Stat
+				title="Categorias"
+				:value="stats.categories"
+				icon="fa fa-folder"
+				color="#d54d50"
+			/>
+			<Stat
+				title="Artigos"
+				:value="stats.articles"
+				icon="fa fa-file"
+				color="#3bc480"
+			/>
+			<Stat
+				title="Usuários"
+				:value="stats.users"
+				icon="fa fa-user"
+				color="#3282cd"
+			/>
+		</div>
 	</div>
 </template>
 
 <script>
-	import PageTitle from '../template/PageTitle';
+import PageTitle from "../template/PageTitle";
+import Stat from "./Stat";
+import axios from "axios";
+import { baseApiUrl } from "@/config/global";
 
-	export default {
-		name: 'Home',
-		components: { PageTitle }
+export default {
+	name: "Home",
+	components: { PageTitle, Stat },
+	data: function() {
+		return {
+			stats: {}
+		};
+	},
+	methods: {
+		getStats() {
+			axios
+				.get(`${baseApiUrl}/stats`)
+				.then(res => (this.stats = res.data))
+				.catch(err => {});
+		}
+	},
+	mounted() {
+		this.getStats();
 	}
+};
 </script>
+
+
+<style>
+	.stats {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+	}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,20 @@
-import 'font-awesome/css/font-awesome.css';
-import Vue from 'vue';
+import "font-awesome/css/font-awesome.css";
+import Vue from "vue";
 
-import App from './App';
+import App from "./App";
 
-import './config/bootstrap';
-import store from './config/store';
-import router from './config/router';
+import "./config/bootstrap";
+import store from "./config/store";
+import router from "./config/router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// TODO: remover
+require("axios").defaults.headers.common["Authorization"] =
+	"bearer " + process.env.VUE_APP_TOKEN;
 
 new Vue({
 	store,
 	router,
-  render: h => h(App)
-}).$mount('#app')
+	render: h => h(App)
+}).$mount("#app");
