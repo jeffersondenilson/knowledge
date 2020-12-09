5f7a306d1df06cd588d9c988635f04ec361cddd9
jeffersondenilson <jefersond121212@gmail.com>
2020-12-09 14:27:44 -0300

    autenticação

---
+++ frontend/src/components/auth/Auth.vue
@@ -0,0 +1,167 @@
+<template>
+	<div class="auth-content">
+		<div class="auth-modal">
+			<img :src="require('@/assets/logo.png')" width="200" alt="Logo" />
+			<hr />
+			<div class="auth-title">
+				{{ showSignUp ? "Cadastro" : "Login" }}
+			</div>
+
+			<b-form>
+				<b-form-group v-if="showSignUp" label="Nome:" label-for="name">
+					<b-form-input
+						id="name"
+						type="text"
+						v-model="user.name"
+						required
+						autocomplete="off"
+						placeholder="Nome"
+					/>
+				</b-form-group>
+				<b-form-group label="E-mail:" label-for="email">
+					<b-form-input
+						id="email"
+						type="text"
+						v-model="user.email"
+						required
+						autocomplete="off"
+						placeholder="E-mail"
+					/>
+				</b-form-group>
+				<b-form-group label="Senha:" label-for="password">
+					<b-form-input
+						id="password"
+						type="password"
+						v-model="user.password"
+						required
+						autocomplete="off"
+						placeholder="Senha"
+					/>
+				</b-form-group>
+				<b-form-group
+					v-if="showSignUp"
+					label="Confirme a Senha:"
+					label-for="confirm-password"
+				>
+					<b-form-input
+						id="confirm-password"
+						type="password"
+						v-model="user.confirmPassword"
+						required
+						autocomplete="off"
+						placeholder="Confirme a Senha"
+					/>
+				</b-form-group>
+			</b-form>
+
+			<!-- <input
+				v-if="showSignUp"
+				v-model="user.name"
+				type="text"
+				placeholder="Nome"
+			/>
+			<input v-model="user.email" type="text" placeholder="E-mail" />
+			<input v-model="user.password" type="password" placeholder="Senha" />
+			<input
+				v-if="showSignUp"
+				v-model="user.confirmPassword"
+				type="password"
+				placeholder="Confirme a Senha"
+			/> -->
+
+			<button v-if="showSignUp" @click="signup" class="btn btn-primary">
+				Registrar
+			</button>
+			<button v-else @click="signin" class="btn btn-primary">Entrar</button>
+
+			<a @click.prevent="showSignUp = !showSignUp">
+				<span v-if="showSignUp">Já tem cadastro? Acesse o Login!</span>
+				<span v-else>Não tem cadastro? Registre-se aqui!</span>
+			</a>
+		</div>
+	</div>
+</template>
+
+<script>
+import { baseApiUrl, showError, userKey } from "@/config/global";
+import axios from "axios";
+
+export default {
+	name: "Auth",
+	data: function() {
+		return {
+			showSignUp: false,
+			user: {}
+		};
+	},
+	methods: {
+		signin() {
+			axios
+				.post(`${baseApiUrl}/signin`, this.user)
+				.then(res => {
+					this.$store.commit("setUser", res.data);
+					localStorage.setItem(userKey, JSON.stringify(res.data));
+					this.$toasted.global.defaultSuccess({ msg: "Usuário logado" });
+					this.$route.push({ path: "/" });
+				})
+				.catch(showError);
+		},
+		signup() {
+			axios
+				.post(`${baseApiUrl}/signup`, this.user)
+				.then(res => {
+					this.$toasted.global.defaultSuccess({ msg: "Usuário criado" });
+					this.user = {};
+					this.showSignUp = false;
+				})
+				.catch(showError);
+		}
+	}
+};
+</script>
+
+<style>
+.auth-content {
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.auth-modal {
+	background-color: #fff;
+	width: 350px;
+	padding: 35px;
+	box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.auth-title {
+	font-size: 1.1rem;
+	font-weight: 100;
+	margin-top: 10px;
+	margin-bottom: 15px;
+}
+
+.auth-modal button {
+	align-self: flex-end;
+}
+
+.auth-modal a {
+	margin-top: 35px;
+}
+
+.auth-modal hr {
+	border: 0;
+	width: 100%;
+	height: 1px;
+	background-image: linear-gradient(
+		to right,
+		rgba(120, 120, 120, 0),
+		rgba(120, 120, 120, 0.75),
+		rgba(120, 120, 120, 0)
+	);
+}
+</style>
--- frontend/src/components/template/UserDropdown.vue
+++ frontend/src/components/template/UserDropdown.vue
@@ -1,68 +1,76 @@
 <template>
 	<b-dropdown class="user-dropdown" boundary="viewport" offset="15">
 		<template #button-content>
-      <span class="d-none d-sm-block">
-      	{{ user.name }}
-      </span>
-      <div>
-      	<Gravatar :email="user.email" alt="User" />
-      </div>
-    </template>
+			<span class="d-none d-sm-block">
+				{{ user.name }}
+			</span>
+			<div>
+				<Gravatar :email="user.email" alt="User" />
+			</div>
+		</template>
 
-	  <b-dropdown-item>
-	  	<router-link to="/admin">
-	  		<i class="fa fa-cogs"></i> Administração
-	  	</router-link>
-	  </b-dropdown-item>
-	  <b-dropdown-item href="#">
-	  	<i class="fa fa-sign-out"></i> Sair
-	  </b-dropdown-item>
-  </b-dropdown>
+		<b-dropdown-item>
+			<router-link to="/admin">
+				<i class="fa fa-cogs"></i> Administração
+			</router-link>
+		</b-dropdown-item>
+		<b-dropdown-item href="#">
+			<i class="fa fa-sign-out"></i> Sair
+		</b-dropdown-item>
+	</b-dropdown>
 </template>
 
 <script>
-	import { mapState } from 'vuex';
-	import Gravatar from 'vue-gravatar';
+import { userKey } from "@/config/global";
+import { mapState } from "vuex";
+import Gravatar from "vue-gravatar";
 
-	export default {
-		name: 'UserDropdown',
-		components: { Gravatar },
-		computed: mapState(['user'])
+export default {
+	name: "UserDropdown",
+	components: { Gravatar },
+	computed: mapState(["user"]),
+	methods: {
+		logout() {
+			localStorage.removeItem(userKey);
+			this.$store.commit("setUser", null);
+			this.$router.push({ name: "auth" });
+		}
 	}
+};
 </script>
 
 <style>
-	.user-dropdown {
-		height: 100%;
-	}
+.user-dropdown {
+	height: 100%;
+}
 
-	.user-dropdown > button {
-		background-color: transparent;
-		display: flex;
-		align-items: center;
-		font-weight: 100;
-		height: 100%;
-		padding: 0px 20px;
-		border: none;
-	}
+.user-dropdown > button {
+	background-color: transparent;
+	display: flex;
+	align-items: center;
+	font-weight: 100;
+	height: 100%;
+	padding: 0px 20px;
+	border: none;
+}
 
-	.user-dropdown > button:hover {
-		background-color: rgba(0, 0, 0, 0.2);
-	}
+.user-dropdown > button:hover {
+	background-color: rgba(0, 0, 0, 0.2);
+}
 
-	.user-dropdown img {
-		margin: 0px 10px;
-		max-height: 37px;
-		border-radius: 5px;
-	}
+.user-dropdown img {
+	margin: 0px 10px;
+	max-height: 37px;
+	border-radius: 5px;
+}
 
-	.user-dropdown a {
-		color: #000;
-		text-decoration: none;
-	}
+.user-dropdown a {
+	color: #000;
+	text-decoration: none;
+}
 
-	.user-dropdown a:hover {
-		color: #000;
-		text-decoration: none;
-	}
+.user-dropdown a:hover {
+	color: #000;
+	text-decoration: none;
+}
 </style>
